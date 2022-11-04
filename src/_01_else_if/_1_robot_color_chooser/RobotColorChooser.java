0f2defbf996893e630f23a2d0e7b5b2fb0e45f8b
league <54760319+OceanNut420@users.noreply.github.com>
2022-11-03 17:02:37 -0700

    Today I did robot color chooser

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -36,9 +36,41 @@ public class RobotColorChooser {
 			rob.setPenColor(Color.magenta);
 		}
 		if(color.equalsIgnoreCase("orange")) {
+			rob.setPenColor(Color.orange);
+		}
+		if(color.equalsIgnoreCase("red")); {
 			rob.setPenColor(Color.red);
 		}
+		if(color.equalsIgnoreCase("blue")) {
+			rob.setPenColor(Color.blue);
 		}
+		if(color.equalsIgnoreCase("teal")) {
+			rob.setPenColor(50, 168, 158);
+		}
+		if(color.equalsIgnoreCase("purple")) {
+			rob.setPenColor(127, 50, 168);
+		}
+		if(color.equalsIgnoreCase("yellow")) {
+			rob.setPenColor(Color.yellow);
+		}
+		if(color.equalsIgnoreCase("black")) {
+			rob.setPenColor(Color.black);
+		}
+		if(color.equalsIgnoreCase("green")) {
+			rob.setPenColor(Color.green);
+		}
+		if(color.equalsIgnoreCase("violet")) {
+			rob.setPenColor(127,50,158);
+		}
+		if(color.equalsIgnoreCase("brown")) {
+			rob.setPenColor(168,111,50);
+		}
+		if(color.equalsIgnoreCase("grey")) {
+			rob.setPenColor(Color.gray);		
+		}
+	    if(color.equalsIgnoreCase("white")) {
+	    	rob.setPenColor(Color.white);
+	    }
 		//6. If the user doesn't enter anything, choose a random color
        
 		//7. Put a loop around your code so that you keep asking the user for more colors & drawing them
@@ -49,4 +81,4 @@ public class RobotColorChooser {
 
 
 	}
-}
+
