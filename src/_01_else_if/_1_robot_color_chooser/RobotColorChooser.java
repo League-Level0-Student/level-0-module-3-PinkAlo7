166ae09ae4b8c608b13adddcd668dedb11924992
league <league@imac-22.attlocal.net>
2022-11-09 16:55:22 -0800

    Today I did the rest of Robot color chooser, crazy cat lady, and high low game

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -13,32 +13,21 @@ public class RobotColorChooser {
 		//1. Create a new Robot
 	   Robot rob = new Robot();
 		//2. Make the robot draw a shape (this will take more than one line of code)		
-	   rob.penDown();
-	   rob.setSpeed(100);
-	   int count = 0;
-       rob.move(200);
-       rob.turn(90);
-       count +=1;    
-       rob.move(200);
-  	   rob.turn(90);
-       count+=1;
-       while(count < 4) {
-   		rob.move(200);
-   		rob.turn(90);
-   		count+=1;
-   	}
+	   
 		//3. Set the pen width to 10
        rob.setPenWidth(10);
-		//4. Ask the user what color pen they would like the robot to draw with
-	 	String color = JOptionPane.showInputDialog("What color pen would you like the robot to draw with?");
+		
+       //4. Ask the user what color pen they would like the robot to draw with
+       for(int i=0; i<5; i++) {
+       String color = JOptionPane.showInputDialog("What color pen would you like the robot to draw with?");
 		//5. Use an if/else statement to set the pen color that the user requested
 		if(color.equalsIgnoreCase("pink")) {
-			rob.setPenColor(Color.magenta);
+			rob.setPenColor(245, 66, 185);
 		}
 		if(color.equalsIgnoreCase("orange")) {
-			rob.setPenColor(Color.orange);
+			rob.setPenColor(245, 155, 66);
 		}
-		if(color.equalsIgnoreCase("red")); {
+		if(color.equalsIgnoreCase("red")) {
 			rob.setPenColor(Color.red);
 		}
 		if(color.equalsIgnoreCase("blue")) {
@@ -71,14 +60,33 @@ public class RobotColorChooser {
 	    if(color.equalsIgnoreCase("white")) {
 	    	rob.setPenColor(Color.white);
 	    }
+	   
+	    
 		//6. If the user doesn't enter anything, choose a random color
-       
+            if(color.equals("")) {
+            	rob.setRandomPenColor();
+            }
 		//7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
-
+            rob.penDown();
+     	   rob.setSpeed(100);
+     	   int count = 0;
+            rob.move(200);
+            rob.turn(90);
+            count +=1;    
+            rob.move(200);
+       	   rob.turn(90);
+            count+=1;
+            while(count < 4) {
+        		rob.move(200);
+        		rob.turn(90);
+        		count+=1;
+        	}
 		
 
 
 
+       } 
 	}
+}
 
