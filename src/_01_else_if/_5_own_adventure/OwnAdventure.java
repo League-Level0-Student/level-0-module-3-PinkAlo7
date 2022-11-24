60d367925c7004cfb7208fc28d1a868b9934d751
league <league@imac-24.attlocal.net>
2022-11-23 16:50:31 -0800

    Today I did the rest of own adventure

--- src/_01_else_if/_5_own_adventure/OwnAdventure.java
+++ src/_01_else_if/_5_own_adventure/OwnAdventure.java
@@ -56,17 +56,54 @@ public class OwnAdventure {
 									+ "\ndisappearing up into the air and when Lele looked up, she saw the rice krispie monster! 'How dare you eat my body little girl!' exlaimed the rice krispie monster. 'I'm sorry! I didn't"
 									+ "\nknow I wasn't supposed to eat you!' yelled Lele in fear. 'The sign is krispie clear!' said the monster in anger. The sign really was krispie clear...IT WAS COVERED IN RICE KRISPIE! "
 									+ "\n'Because you ate my body I will eat yours!!!' yelled the monster. The rice krispie monster ran after Lele and ate her...THE END!");
-				}if(sword ==0) {
-					int art = JOptionPane.showOptionDialog(null, 
+				}
+				if (sword == 0) {
+					int art = JOptionPane.showOptionDialog(null,
 							"Lele decided she wanted to go to Art Land. Art Land did sound fun, she wanted to color pictures of dragons. When Lele entered Art Land, it was full of markers coloring pencils, pencils, paint, lots"
-							+ "\n of paper, pencil sharpeners, and all kinds of things you would need to color! Lele quickly got some markers and started drawing dragons. She was very good at drawing dragons. She stayed in Art "
-							+ "\nLand for an hour coloring dragons, and when she thought she had drawn enough dragons, she packed some markers, paint brushes, and her drawings and continued on her journey. "
-							+ "\nThe next land she saw was sleepy land. This land was full of beds, stars, moons, pajamas, pillows, and sleeping masks. All of a sudden Lele was feeling sleepy. She really wanted to go to sleep"
-							+ "when she saw all of the comfy beds. What do you think...should she sleep or keep making her way to dragon land? ",
+									+ "\n of paper, pencil sharpeners, and all kinds of things you would need to color! Lele quickly got some markers and started drawing dragons. She was very good at drawing dragons. She stayed in Art "
+									+ "\nLand for an hour coloring dragons, and when she thought she had drawn enough dragons, she packed some markers, paint brushes, and her drawings and continued on her journey. "
+									+ "\nThe next land she saw was sleepy land. This land was full of beds, stars, moons, pajamas, pillows, and sleeping masks. All of a sudden Lele was feeling sleepy. She really wanted to go to sleep"
+									+ "\nwhen she saw all of the comfy beds. What do you think...should she sleep or keep making her way to Dragon Land? ",
+							"The Dragon Girl", 0, JOptionPane.INFORMATION_MESSAGE, null,
+							new String[] { "Keep making her way to Dragon Land", "Sleep" }, null);
+					if (art == 0) {
+						JOptionPane.showMessageDialog(null,
+								"Even though Lele was very tired, she still wanted to get a dragon as soon as possible. So Lele continued on her journey, but right when she was about to leave sleepy"
+										+ "\nland, she saw a sign that said,'if you don't sleep in 5 seconds you will die!' Lele laughed. 'I would never die just because I didn't sleep' said Lele, but suddenly, she fell to the ground "
+										+ "\nand was in a deep sleep...FOREVER!...THE END!");
+					}
+					if (art == 1) {
+						int sleep = JOptionPane.showOptionDialog(null,
+								"Lele cound't help it, so she found the most comfy bed she could find and immediately started sleeping."
+										+ "\nThe next day, Lele got up and packed her bags and continued on her journey. The next land she came to was Cheese Land. This land was full of all types of cheese: Gooda cheese, chedar cheese, "
+										+ "\nstring cheese, goat cheese, and muche more! Lele hadn't eaten anything that morning and ran to the cheese and started eating. While Lele was eating, she heard a rumble on the ground and little "
+										+ "\nsqeaks everywhere. Suddenly, thousands of mice ran towards her and pushed her and ate the cheese. Lele was being carried by the mice evrywhere she didn't get another chance to eat another bite"
+										+ "\nof cheese. The mice kept carrying her everywhere so fast that Lele fell off of the mice. Lele wanted to eat the cheese so badly, but the mice were all over the cheese that you couldn't see the "
+										+ "\ncheese. Should Lele try to get some cheese to eat or keep making her way to Dragon Land?",
+								"The Dragon Girl", 0, JOptionPane.INFORMATION_MESSAGE, null,
+								new String[] { "Get some cheese", " Keep making her way to Dragon Land" }, null);
+						if (sleep == 1) {
+							JOptionPane.showMessageDialog(null,
+									"Even though Lele was so hungry, she still wanted to get a dragon as soon as possible. so she continued on her journey and looked to see if she happened to see and other food lands"
+											+ "\nbut Cheese Land was the last land she had seen that had food. Lele's stomach was growling so much. She suddenly fell to the ground and died from hunger...THE END!");
+						}if (sleep == 0) {
+								JOptionPane.showMessageDialog(null,
+										"Lele was very hungry, so she ran towards the cheese and pushed the mice out of her way and climbed on top of the cheese. She grabbed as much cheese as she could and when she grabbed as much as she "
+												+ "\ncould, she jumped off the cheese and ran towards an area without mice. she then continued on her journey. The next land was....DRAGON LAND! She had finally made it to Dragon Land. There were so"
+												+ "\nmany dragons that were so beautiful that she didn't know which one to pick! Lele was so exited that she had found Dragon Land and ran towards the dragons and pet them. She wanted to fly on them,"
+												+ "\nso she hopped on a dragon and immediately soared into the air. She could see a view of all of the lands she had went through. Then she landed the dragon and looked very sad. 'I wish I could take "
+												+ "\nall of these dragons, but I can only ride one of them home. Then Lele thought of an idea,'Wait! if I want all of the dragons then I could live with them! I do always feel lonely at home, and all"
+												+ "\nof these dragons are so beautiful!' Lele exlaimed. "
+												+ "\nSo Lele decided that she was going to live with the dragons and lived happily ever after, the end. ");
+
+							}
+						}
+
+					}
 				}
 
 			}
 
 		}
 	}
-}
+
