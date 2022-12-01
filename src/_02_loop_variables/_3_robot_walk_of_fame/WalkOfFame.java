ac154607edc8bee6e8d1d72468dd9cb6e98c8c3b
Jebbers7 <derinkaya52@gmail.com>
2022-11-30 16:56:46 -0800

    11/30/22 Today I did years alive, shiny objects, robot walk of fame, 99 bottles

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -6,13 +6,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		
-		// 1. Set the X position of the robot so that it starts on the left. 
+
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
+		int X = 150;
 
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
-		// 3. Set the length of each line in the star to 30.
+		for (int i = 0; i < 11; i++) {
+			rob.setX(X);
+			rob.setY(100);
+			rob.penDown();
+			rob.setSpeed(100);
+			for (int h = 0; h < 6; h++) {
+				rob.turn(144);
+				// 3. Set the length of each line in the star to 30.
+				rob.move(30);
+				
+			}
+			X += 150;
+		}
+		rob.hide();
 
 		/** THE CHALLENGE: **/
 		/*
---
+++ src/yearsalive.java
@@ -0,0 +1,13 @@
+
+public class yearsalive {
+
+public static void main(String[] args) {
+	
+
+for(int i=2012 ; i<2023; i++) {
+	System.out.println(i);
+
+}
+}
+}
+
